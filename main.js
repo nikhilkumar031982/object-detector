9a8bca3cc02cccff7e2c30378b5bb477f2c7af88
nikhilkumar031982 <91476583+nikhilkumar031982@users.noreply.github.com>
2022-05-12 17:52:16 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+
+function open1(){
+    window.location.href="index copy 2.html";
+}
+
+function open2(){
+    window.location.href="index copy 3.html";
+}
+
+function open3(){
+    window.location.href="index copy 4.html";
+}
+
+function open4(){
+    window.location.href="index copy 5.html";
+}
